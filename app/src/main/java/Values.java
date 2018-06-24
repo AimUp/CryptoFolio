f563748f4c571abeba15297836cdea6f8d492c71
Aimar Ugarte <ugarteaimar@gmail.com>
2018-06-24 21:55:22 +0200

    estilo de home

---
+++ app/src/main/java/Values.java
@@ -0,0 +1,7 @@
+public class Values {
+
+    //URLs
+    public static String github = "http://www.githun.com/augarte";
+    public static String githubRepo = "http://www.githun.com/augarte/Cryptofolio";
+
+}
--- app/src/main/java/com/aimarugarte/cryptofolio/Fragments/AboutFragment.java
+++ app/src/main/java/com/aimarugarte/cryptofolio/Fragments/AboutFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.aimarugarte.cryptofolio.Main2Activity;
 import com.aimarugarte.cryptofolio.R;
@@ -14,9 +15,17 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        final View view = inflater.inflate(R.layout.fragment_about, container, false);
         ((Main2Activity) getActivity()).getSupportActionBar().setTitle("About");
-        
+
+        Button github_btn = (Button) view.findViewById(R.id.github_btn);
+        github_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         return view;
     }
 
